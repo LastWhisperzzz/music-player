050560d7fabb85f17eac8e0cdc3ce7c59c1f4fe7
Last Whisper <xxzworkmail@gmail.com>
2020-11-11 03:06:05 +0800

    edit vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/music/" : "./",
-    outputDir: "music",
-  };
-  
+    publicPath: process.env.NODE_ENV == 'production' ? './' : './',
+    outputDir: 'dist',
+    indexPath: 'index.html',
+    filenameHashing: true,
+    lintOnSave: process.env.NODE_ENV === 'production',
+    runtimeCompiler: false,
+    productionSourceMap: true
+}
