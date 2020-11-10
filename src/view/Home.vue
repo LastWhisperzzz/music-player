2d2c50bb4e6f658074848e158a89bbe7f2524c38
Last Whisper <xxzworkmail@gmail.com>
2020-11-11 01:25:36 +0800

    complete

---
+++ src/view/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <player />
+  </div>
+</template>
+
+<script>
+import Player from "../components/Player";
+export default {
+  components: {
+    Player
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+
+  z-index: 0;
+
+  position: relative;
+  justify-content: center;
+  align-items: center;
+}
+.mytips {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.project_img {
+  width: 20%;
+  height: 20%;
+  float: left;
+}
+.mytips p {
+  width: 60%;
+  float: left;
+}
+</style>
